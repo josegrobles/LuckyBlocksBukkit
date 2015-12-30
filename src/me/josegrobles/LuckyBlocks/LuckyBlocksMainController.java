5f2325f40705eded7bfbb3d4d462dc629e162d3c
Jose Antonio González Robles <pinguisl@yahoo.es>
2015-12-30 13:51:45 +0100

    1. Added unbreakability against TNT and Creeper Explosions. 2. Added automatic download of Texture Pack 3. Added new mehods. 4. Fixed some Lucky Blocks.

--- src/me/josegrobles/LuckyBlocks/LuckyBlocksMainController.java
+++ src/me/josegrobles/LuckyBlocks/LuckyBlocksMainController.java
@@ -1,23 +1,84 @@
 package me.josegrobles.LuckyBlocks;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.logging.Level;
+
 
 /**
  * Created by Jose on 24/12/15.
  */
-public class LuckyBlocksMainController extends JavaPlugin implements Listener{
+public class LuckyBlocksMainController extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+
     }
     @EventHandler
-    public void LuckyBlocks(BlockBreakEvent event){
-        if (event.getBlock().getType().equals(Material.SPONGE)){
-            new LuckyBlocksProbabilityClass().selection(event);
+    public void PlayerJoin(final PlayerJoinEvent e){
+        e.getPlayer().sendMessage("§6Cargando Pack de Texturas...");
+
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            public void run() {
+
+                e.getPlayer().setResourcePack("http://bicimap.azurewebsites.net/LuckyBlocks.zip");
+            }
+        }, 20L);
+
+
+    }
+
+
+
+    @EventHandler
+    public void LuckyBlocks(BlockBreakEvent event) {
+        if (event.getBlock().getType().equals(Material.SPONGE)) {
+            event.setCancelled(true);
+            event.getBlock().setType(Material.AIR);
+            new LuckyBlocksProbabilityClass().Selection(event);
+        }
+    }
+
+    @EventHandler
+    public void coinDropEvent(PlayerDropItemEvent event) {
+        if (event.getItemDrop().getName().equalsIgnoreCase("item.item.goldnugget")) {
+            new LuckyBlockActionController().coinCheck(event);
+        }
+    }
+
+    @EventHandler
+    public void chestOpenEvent(PlayerInteractEvent event) {
+        if (event.getClickedBlock() == null) return;
+        else if (event.getClickedBlock().getType() == Material.CHEST && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            new LuckyBlockActionController().UnluckyMagicChest(event);
+        }
+    }
+    @EventHandler
+    public void onTNTExplosion(EntityExplodeEvent event) {
+        if(event.getEntityType() == EntityType.PRIMED_TNT || event.getEntityType() == EntityType.CREEPER) {
+            List<Block> blocks = event.blockList();
+            for(int i=0; i < blocks.size(); i++) {
+                Block b = blocks.get(i);
+                if(b.getType() == Material.SPONGE)
+                    event.blockList().remove(b);
+            }
         }
     }
 }
+
+
+
--- src/me/josegrobles/LuckyBlocks/LuckyBlocksProbabilityClass.java
+++ src/me/josegrobles/LuckyBlocks/LuckyBlocksProbabilityClass.java
@@ -7,18 +7,24 @@ import org.bukkit.event.block.BlockBreakEvent;
  */
 
 public class LuckyBlocksProbabilityClass {
-    public void selection(BlockBreakEvent e){
+    public void Selection(BlockBreakEvent e){
         int RandomSelection = (int)(Math.random()*100);
-        if (RandomSelection >= 0 && RandomSelection <= 15) new LuckyBlockActionController().LuckyPotion(e);
-        else if (RandomSelection >= 16 && RandomSelection <= 35) new LuckyBlockActionController().EnchantedSword(e);
-        else if (RandomSelection >= 36 && RandomSelection <= 63) new LuckyBlockActionController().armour(e);
-        else if (RandomSelection >= 64 && RandomSelection <= 72){
+        if (RandomSelection >= 0 && RandomSelection <= 10) new LuckyBlockActionController().LuckyPotion(e);
+        else if (RandomSelection >= 11 && RandomSelection <= 15) new LuckyBlockActionController().EnchantedSword(e);
+        else if (RandomSelection >= 16 && RandomSelection <= 20) new LuckyBlockActionController().EnchantedBow(e);
+        else if (RandomSelection >= 21 && RandomSelection <= 40) new LuckyBlockActionController().RandomItems(e);
+        else if (RandomSelection >= 41 && RandomSelection <= 52) new LuckyBlockActionController().armour(e);
+        else if (RandomSelection >= 53 && RandomSelection <= 67){
             int spawnQuantity = (int)(Math.random()*10);
             new LuckyBlockActionController().SpawnEnemyEntities(e,spawnQuantity);
         }
-        else if (RandomSelection >= 73 && RandomSelection <= 82) new LuckyBlockActionController().AnvilDrop(e);
-        else if (RandomSelection >= 83 && RandomSelection <= 90) new LuckyBlockActionController().lavaDrop(e);
-        else if (RandomSelection >= 91 && RandomSelection <= 96) new LuckyBlockActionController().PrimeTNT(e);
+        else if (RandomSelection >= 68 && RandomSelection <= 70) new LuckyBlockActionController().AnvilDrop(e);
+        else if (RandomSelection >= 71 && RandomSelection <= 74) new LuckyBlockActionController().lavaDrop(e);
+        else if (RandomSelection >= 75 && RandomSelection <= 82) new LuckyBlockActionController().LuckyPit(e);
+        else if (RandomSelection >= 83 && RandomSelection <= 87) new LuckyBlockActionController().PrimeTNT(e);
+        else if (RandomSelection >= 88 && RandomSelection <= 90) new LuckyBlockActionController().waterStructure(e);
+        else if (RandomSelection >= 91 && RandomSelection <= 94) new LuckyBlockActionController().MobApocalypyse(e);
+        else if (RandomSelection >= 95 && RandomSelection <= 97) new LuckyBlockActionController().MagicChest(e);
         else new LuckyBlockActionController().infiniteDestruction(e);
 
 
